8f56fd3820571f1852c3af98d10d0bd8804e31f1
Nadiia Semenchuk <nadiiasem@gmail.com>
2015-10-03 17:16:17 -0400

    Project 4

---
+++ 6_2_34_SemenchukNadiia/frmAvrGrd.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAvrGrd
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.btnCompute = New System.Windows.Forms.Button()
+    Me.txtAverage = New System.Windows.Forms.TextBox()
+    Me.lblAvg = New System.Windows.Forms.Label()
+    Me.lblName = New System.Windows.Forms.Label()
+    Me.btnQuit = New System.Windows.Forms.Button()
+    Me.lblAdd = New System.Windows.Forms.Label()
+    Me.SuspendLayout()
+    '
+    'btnCompute
+    '
+    Me.btnCompute.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+    Me.btnCompute.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.btnCompute.Location = New System.Drawing.Point(58, 25)
+    Me.btnCompute.Name = "btnCompute"
+    Me.btnCompute.Size = New System.Drawing.Size(204, 57)
+    Me.btnCompute.TabIndex = 0
+    Me.btnCompute.Text = "Get Five Grades and Compute Average"
+    Me.btnCompute.UseVisualStyleBackColor = False
+    '
+    'txtAverage
+    '
+    Me.txtAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.txtAverage.Location = New System.Drawing.Point(173, 114)
+    Me.txtAverage.Multiline = True
+    Me.txtAverage.Name = "txtAverage"
+    Me.txtAverage.ReadOnly = True
+    Me.txtAverage.Size = New System.Drawing.Size(63, 25)
+    Me.txtAverage.TabIndex = 1
+    '
+    'lblAvg
+    '
+    Me.lblAvg.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.lblAvg.Location = New System.Drawing.Point(55, 104)
+    Me.lblAvg.Name = "lblAvg"
+    Me.lblAvg.Size = New System.Drawing.Size(69, 21)
+    Me.lblAvg.TabIndex = 2
+    Me.lblAvg.Text = "Average: "
+    Me.lblAvg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+    '
+    'lblName
+    '
+    Me.lblName.AutoSize = True
+    Me.lblName.Location = New System.Drawing.Point(3, 181)
+    Me.lblName.Name = "lblName"
+    Me.lblName.Size = New System.Drawing.Size(111, 13)
+    Me.lblName.TabIndex = 3
+    Me.lblName.Text = "by Nadiia Semenchuk"
+    '
+    'btnQuit
+    '
+    Me.btnQuit.Location = New System.Drawing.Point(232, 168)
+    Me.btnQuit.Name = "btnQuit"
+    Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+    Me.btnQuit.TabIndex = 4
+    Me.btnQuit.Text = "Exit"
+    Me.btnQuit.UseVisualStyleBackColor = True
+    '
+    'lblAdd
+    '
+    Me.lblAdd.Location = New System.Drawing.Point(55, 125)
+    Me.lblAdd.Name = "lblAdd"
+    Me.lblAdd.Size = New System.Drawing.Size(90, 31)
+    Me.lblAdd.TabIndex = 5
+    Me.lblAdd.Text = "(after dropping the lowest grade)"
+    '
+    'frmAvrGrd
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(319, 203)
+    Me.Controls.Add(Me.lblAdd)
+    Me.Controls.Add(Me.btnQuit)
+    Me.Controls.Add(Me.lblName)
+    Me.Controls.Add(Me.lblAvg)
+    Me.Controls.Add(Me.txtAverage)
+    Me.Controls.Add(Me.btnCompute)
+    Me.Name = "frmAvrGrd"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Average Grade"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents btnCompute As System.Windows.Forms.Button
+  Friend WithEvents txtAverage As System.Windows.Forms.TextBox
+  Friend WithEvents lblAvg As System.Windows.Forms.Label
+  Friend WithEvents lblName As System.Windows.Forms.Label
+  Friend WithEvents btnQuit As System.Windows.Forms.Button
+  Friend WithEvents lblAdd As System.Windows.Forms.Label
+
+End Class
---
+++ 6_2_34_SemenchukNadiia/frmAvrGrd.vb
@@ -0,0 +1,60 @@
+﻿'Project: 6_2_34
+'Description: Program asks user for five grades and returns their average. 
+'Programmer: Nadiia Semenchuk
+'Date: October 5, 2014
+
+'I promise that I created this code and I did not copy from someone else or the internet.
+
+
+Public Class frmAvrGrd
+
+  Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+    'Closes form
+    Me.Close()
+  End Sub
+
+  Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+    'Asks user for five grades using loops and computes grade's average. 
+    Dim gradeSum As Integer = 0
+    Dim gradeAvg As Double = 0
+    Dim num As Integer = 0
+    Dim min As Integer = 100
+
+    For i As Integer = 1 To 5
+      'Main loop, asks user 5 times for a grade
+      Dim msg As String = "Enter your grade # " & i.ToString & " please: "
+      Dim title As String = "Grade"
+      Dim response As String = ""
+
+      response = InputBox(msg, title)
+
+      Do While IsNumeric(response) = False
+        'First inner loop, checks if user's input is numeric
+        MessageBox.Show("Sorry, please enter only numbers.")
+        response = InputBox(msg, title)
+      Loop
+
+      num = CInt(response)
+
+      Do While num < 0 Or num > 100
+        'Second inner loop, checks if user's input is in valid grade's range
+        MessageBox.Show("Please enter your grade value from 1 to 100.")
+        response = InputBox(msg, title)
+        num = CInt(response)
+      Loop
+
+      If num < min Then
+        'looks for the smallest entered number
+        min = num
+      End If
+
+      gradeSum += num 'Adds all entered numbers
+    Next
+
+    'Computes and displays average grade. Value of variable min is substracted 
+    'from the total sum. That difference is the sum without the lowest grade. 
+    gradeAvg = (gradeSum - min) / 4
+    txtAverage.Text = gradeAvg.ToString("n2")
+
+  End Sub
+End Class
