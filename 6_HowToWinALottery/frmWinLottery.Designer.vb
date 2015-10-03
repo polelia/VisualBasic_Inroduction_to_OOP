7da653d00eb898af475ca514fb0e1a341de3a256
Nadiia Semenchuk <nadiiasem@gmail.com>
2015-10-03 17:20:08 -0400

    Project 5

---
+++ 6_HowToWinALottery/frmWinLottery.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWinLottery
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Me.chkScratchOff = New System.Windows.Forms.CheckBox()
+        Me.chkDraw = New System.Windows.Forms.CheckBox()
+        Me.btnGetTickets = New System.Windows.Forms.Button()
+        Me.lblQuestion = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblOptions = New System.Windows.Forms.Label()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.mtbAmount = New System.Windows.Forms.MaskedTextBox()
+        Me.grbScratch = New System.Windows.Forms.GroupBox()
+        Me.chkBingo = New System.Windows.Forms.CheckBox()
+        Me.chkInferno = New System.Windows.Forms.CheckBox()
+        Me.chkRoyal = New System.Windows.Forms.CheckBox()
+        Me.grbDraw = New System.Windows.Forms.GroupBox()
+        Me.chkMega = New System.Windows.Forms.CheckBox()
+        Me.chkPowerball = New System.Windows.Forms.CheckBox()
+        Me.chkFlorida = New System.Windows.Forms.CheckBox()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.grbScratch.SuspendLayout()
+        Me.grbDraw.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'chkScratchOff
+        '
+        Me.chkScratchOff.AutoSize = True
+        Me.chkScratchOff.Location = New System.Drawing.Point(65, 137)
+        Me.chkScratchOff.Name = "chkScratchOff"
+        Me.chkScratchOff.Size = New System.Drawing.Size(85, 17)
+        Me.chkScratchOff.TabIndex = 1
+        Me.chkScratchOff.Text = "Scratch-Offs"
+        Me.chkScratchOff.UseVisualStyleBackColor = True
+        '
+        'chkDraw
+        '
+        Me.chkDraw.AutoSize = True
+        Me.chkDraw.Location = New System.Drawing.Point(65, 224)
+        Me.chkDraw.Name = "chkDraw"
+        Me.chkDraw.Size = New System.Drawing.Size(87, 17)
+        Me.chkDraw.TabIndex = 2
+        Me.chkDraw.Text = "Draw Games"
+        Me.chkDraw.UseVisualStyleBackColor = True
+        '
+        'btnGetTickets
+        '
+        Me.btnGetTickets.Location = New System.Drawing.Point(1, 279)
+        Me.btnGetTickets.Name = "btnGetTickets"
+        Me.btnGetTickets.Size = New System.Drawing.Size(105, 51)
+        Me.btnGetTickets.TabIndex = 5
+        Me.btnGetTickets.Text = "Get my tickets!"
+        Me.btnGetTickets.UseVisualStyleBackColor = True
+        '
+        'lblQuestion
+        '
+        Me.lblQuestion.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblQuestion.Location = New System.Drawing.Point(36, 21)
+        Me.lblQuestion.Name = "lblQuestion"
+        Me.lblQuestion.Size = New System.Drawing.Size(160, 52)
+        Me.lblQuestion.TabIndex = 7
+        Me.lblQuestion.Text = "How much money do you" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "wish to spend" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "on lottery tickets?"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(9, 525)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(51, 13)
+        Me.lblName.TabIndex = 8
+        Me.lblName.Text = "by Nadiia"
+        '
+        'lblOptions
+        '
+        Me.lblOptions.AutoSize = True
+        Me.lblOptions.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOptions.Location = New System.Drawing.Point(26, 90)
+        Me.lblOptions.Name = "lblOptions"
+        Me.lblOptions.Size = New System.Drawing.Size(124, 15)
+        Me.lblOptions.TabIndex = 9
+        Me.lblOptions.Text = "Choose your options: "
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(12, 427)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuit.TabIndex = 12
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(12, 385)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'mtbAmount
+        '
+        Me.mtbAmount.Location = New System.Drawing.Point(202, 37)
+        Me.mtbAmount.Mask = "00000"
+        Me.mtbAmount.Name = "mtbAmount"
+        Me.mtbAmount.Size = New System.Drawing.Size(59, 20)
+        Me.mtbAmount.TabIndex = 14
+        '
+        'grbScratch
+        '
+        Me.grbScratch.Controls.Add(Me.chkBingo)
+        Me.grbScratch.Controls.Add(Me.chkInferno)
+        Me.grbScratch.Controls.Add(Me.chkRoyal)
+        Me.grbScratch.Location = New System.Drawing.Point(202, 96)
+        Me.grbScratch.Name = "grbScratch"
+        Me.grbScratch.Size = New System.Drawing.Size(235, 83)
+        Me.grbScratch.TabIndex = 15
+        Me.grbScratch.TabStop = False
+        Me.grbScratch.Text = "Scratch-Off"
+        Me.grbScratch.Visible = False
+        '
+        'chkBingo
+        '
+        Me.chkBingo.AutoSize = True
+        Me.chkBingo.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkBingo.Location = New System.Drawing.Point(9, 56)
+        Me.chkBingo.Name = "chkBingo"
+        Me.chkBingo.Size = New System.Drawing.Size(202, 21)
+        Me.chkBingo.TabIndex = 2
+        Me.chkBingo.Text = "Bingo Gold (win up to $50,000)"
+        Me.chkBingo.UseVisualStyleBackColor = True
+        '
+        'chkInferno
+        '
+        Me.chkInferno.AutoSize = True
+        Me.chkInferno.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkInferno.Location = New System.Drawing.Point(9, 35)
+        Me.chkInferno.Name = "chkInferno"
+        Me.chkInferno.Size = New System.Drawing.Size(212, 21)
+        Me.chkInferno.TabIndex = 1
+        Me.chkInferno.Text = "Cash Inferno (win up to $30,000)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.chkInferno.UseVisualStyleBackColor = True
+        '
+        'chkRoyal
+        '
+        Me.chkRoyal.AutoSize = True
+        Me.chkRoyal.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkRoyal.Location = New System.Drawing.Point(9, 16)
+        Me.chkRoyal.Name = "chkRoyal"
+        Me.chkRoyal.Size = New System.Drawing.Size(195, 21)
+        Me.chkRoyal.TabIndex = 0
+        Me.chkRoyal.Text = "Royal 8's  (win up to $10,000)"
+        Me.chkRoyal.UseVisualStyleBackColor = True
+        '
+        'grbDraw
+        '
+        Me.grbDraw.Controls.Add(Me.chkMega)
+        Me.grbDraw.Controls.Add(Me.chkPowerball)
+        Me.grbDraw.Controls.Add(Me.chkFlorida)
+        Me.grbDraw.Location = New System.Drawing.Point(202, 185)
+        Me.grbDraw.Name = "grbDraw"
+        Me.grbDraw.Size = New System.Drawing.Size(247, 87)
+        Me.grbDraw.TabIndex = 16
+        Me.grbDraw.TabStop = False
+        Me.grbDraw.Text = "Draw Games"
+        Me.grbDraw.Visible = False
+        '
+        'chkMega
+        '
+        Me.chkMega.AutoSize = True
+        Me.chkMega.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkMega.Location = New System.Drawing.Point(9, 60)
+        Me.chkMega.Name = "chkMega"
+        Me.chkMega.Size = New System.Drawing.Size(244, 21)
+        Me.chkMega.TabIndex = 2
+        Me.chkMega.Text = "Mega Millions (jackpot $150,000,000)"
+        Me.chkMega.UseVisualStyleBackColor = True
+        '
+        'chkPowerball
+        '
+        Me.chkPowerball.AutoSize = True
+        Me.chkPowerball.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkPowerball.Location = New System.Drawing.Point(9, 39)
+        Me.chkPowerball.Name = "chkPowerball"
+        Me.chkPowerball.Size = New System.Drawing.Size(215, 21)
+        Me.chkPowerball.TabIndex = 1
+        Me.chkPowerball.Text = "Powerball (jackpot $80,000,000)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.chkPowerball.UseVisualStyleBackColor = True
+        '
+        'chkFlorida
+        '
+        Me.chkFlorida.AutoSize = True
+        Me.chkFlorida.Font = New System.Drawing.Font("Franklin Gothic Medium", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkFlorida.Location = New System.Drawing.Point(9, 19)
+        Me.chkFlorida.Name = "chkFlorida"
+        Me.chkFlorida.Size = New System.Drawing.Size(229, 21)
+        Me.chkFlorida.TabIndex = 0
+        Me.chkFlorida.Text = "Florida Lotto (jackpot $34,000,000)"
+        Me.chkFlorida.UseVisualStyleBackColor = True
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.Font = New System.Drawing.Font("Courier New", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 15
+        Me.lstDisplay.Location = New System.Drawing.Point(123, 279)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(355, 259)
+        Me.lstDisplay.TabIndex = 17
+        '
+        'frmWinLottery
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(483, 544)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.grbDraw)
+        Me.Controls.Add(Me.grbScratch)
+        Me.Controls.Add(Me.mtbAmount)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.lblOptions)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblQuestion)
+        Me.Controls.Add(Me.btnGetTickets)
+        Me.Controls.Add(Me.chkDraw)
+        Me.Controls.Add(Me.chkScratchOff)
+        Me.Name = "frmWinLottery"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Win a Lottery"
+        Me.grbScratch.ResumeLayout(False)
+        Me.grbScratch.PerformLayout()
+        Me.grbDraw.ResumeLayout(False)
+        Me.grbDraw.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+  Friend WithEvents chkScratchOff As System.Windows.Forms.CheckBox
+  Friend WithEvents chkDraw As System.Windows.Forms.CheckBox
+  Friend WithEvents btnGetTickets As System.Windows.Forms.Button
+  Friend WithEvents lblQuestion As System.Windows.Forms.Label
+  Friend WithEvents lblName As System.Windows.Forms.Label
+  Friend WithEvents lblOptions As System.Windows.Forms.Label
+  Friend WithEvents btnQuit As System.Windows.Forms.Button
+  Friend WithEvents btnClear As System.Windows.Forms.Button
+  Friend WithEvents mtbAmount As System.Windows.Forms.MaskedTextBox
+  Friend WithEvents grbScratch As System.Windows.Forms.GroupBox
+  Friend WithEvents chkBingo As System.Windows.Forms.CheckBox
+  Friend WithEvents chkInferno As System.Windows.Forms.CheckBox
+  Friend WithEvents chkRoyal As System.Windows.Forms.CheckBox
+  Friend WithEvents grbDraw As System.Windows.Forms.GroupBox
+  Friend WithEvents chkMega As System.Windows.Forms.CheckBox
+  Friend WithEvents chkPowerball As System.Windows.Forms.CheckBox
+  Friend WithEvents chkFlorida As System.Windows.Forms.CheckBox
+  Friend WithEvents lstDisplay As System.Windows.Forms.ListBox
+
+End Class
---
+++ 6_HowToWinALottery/frmWinLottery.vb
@@ -0,0 +1,214 @@
+﻿'GroupProject: 6_HowToWinALottery
+'Description: Program asks a user if he/her wishes to win a lottery, takes his/her input,
+'             and returns info about user's order.
+'Date: October 12, 2014
+
+
+
+Public Class frmWinLottery
+
+  Dim change As Integer = 0
+  Dim ticket1, ticket2, ticket3 As Integer
+  Dim amount As Integer = 0
+  Dim checkedScratchTotal As Integer = 0
+  Dim input As String = ""
+
+  Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+    'Closes form
+    Me.Close()
+  End Sub
+
+
+  Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+    'Clears previously selected controls
+    mtbAmount.Clear()
+    chkDraw.Checked = False
+    chkScratchOff.Checked = False
+    chkBingo.Checked = False
+    chkFlorida.Checked = False
+    chkInferno.Checked = False
+    chkMega.Checked = False
+    chkPowerball.Checked = False
+        chkRoyal.Checked = False
+        ticket1 = 0
+        ticket2 = 0
+        ticket3 = 0
+    lstDisplay.Items.Clear()
+
+  End Sub
+
+  Private Sub frmWinLottery_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    'Asks user if he/her wants to win a lottery and checks his/her inputs
+    Dim title As String = "Your Choice"
+    Dim msg As String = "Do you wish to have a shot at winning a lottery? Answer (Yes or No)"
+    Dim ans As String = InputBox(msg, title).ToUpper
+
+    Do Until ans = "YES" Or ans = "NO" Or ans = "Y" Or ans = "N"
+      MessageBox.Show("Please, enter Yes or No.")
+      ans = InputBox(msg, title).ToUpper
+    Loop
+
+    If ans = "NO" Or ans = "N" Then
+      MessageBox.Show("If so, go back to work/study.")
+      Me.Close()
+    End If
+
+  End Sub
+  Private Sub chkScratchOff_CheckedChanged(sender As Object, e As EventArgs) Handles chkScratchOff.CheckedChanged
+    'Makes controls visible when checkboxes are checked
+    If chkScratchOff.Checked Then
+      grbScratch.Visible = True
+    Else
+      grbScratch.Visible = False
+    End If
+  End Sub
+
+
+  Private Sub chkDraw_CheckedChanged(sender As Object, e As EventArgs) Handles chkDraw.CheckedChanged
+    'Makes controls visible when checkboxes are checked
+    If chkDraw.Checked Then
+      grbDraw.Visible = True
+    Else
+      grbDraw.Visible = False
+    End If
+  End Sub
+ 
+ 
+  Private Sub btnGetTickets_Click(sender As Object, e As EventArgs) Handles btnGetTickets.Click
+
+    input = mtbAmount.Text.Trim
+    'Cheks if user entered dolar amount
+    If input = "" Then
+      MessageBox.Show("Please enter dollar amount.")
+    Else
+      amount = CInt(input)
+
+      'User input validation lower level
+      If amount <= 0 Then
+        MessageBox.Show("Please check if you have enough money for purchase.")
+      End If
+
+      'User input validation upper level
+      If amount > 10000 Then
+        MessageBox.Show("Please don't spend more than you should on lotteries. Seek help. ")
+      End If
+
+
+      If chkScratchOff.Checked Or chkDraw.Checked Then
+        Games(amount)
+      Else
+        MessageBox.Show("Please, make your choice.")
+      End If
+
+      'Royal's 8 = 1
+      'Cash inferno = 2
+      'Bingo Gold = 5
+      'Florida lotto = 1
+      'Powerball = 2
+      'Mega Millions = 5
+      
+      ListOutput()
+    End If
+  End Sub
+
+    Sub Games(ByRef am As Integer)
+    'Calculates number of selected tickets that can be bought on user's input
+    If chkBingo.Checked Or chkMega.Checked Then '$5 tickets
+
+      'Makes sure that, if selected, at least one ticket for $2 will be bought.
+      If chkInferno.Checked Or chkPowerball.Checked Then
+        am -= 2
+        ticket2 = 1
+      End If
+      'If $1 ticket is selected at least 1 will be bought.
+      If chkFlorida.Checked Or chkRoyal.Checked Then
+        am -= 1
+        ticket3 = 1
+      End If
+
+      Do While am >= 5
+        am -= 5  'looks how many $5 tickets can be bought
+        ticket1 += 1 'counter
+        change = am  'assigns change that is left
+      Loop
+    End If
+
+    If chkInferno.Checked Or chkPowerball.Checked Then '$2 tickets
+
+      'In a case when $5 tickets weren't selected and 1$ tickets where selected it reserves one $1 ticket.
+      If (chkFlorida.Checked Or chkRoyal.Checked) And
+          ((chkBingo.Checked = False) Or (chkMega.Checked = False)) Then
+        am -= 1
+        ticket3 = 1
+      End If
+
+      'takes what is left (if previous chkboxes were selected) and looks how many $2 tickets can be bought. 
+      Do While am >= 2
+        am -= 2
+        ticket2 += 1
+        change = am
+      Loop
+    End If
+
+    If chkRoyal.Checked Or chkFlorida.Checked Then '$1 tickets
+      'if something was left (or if these boxes are the only one selected) calculates 1$ tickets
+      Do While am > 0
+        am -= 1
+        ticket3 += 1
+        change = 0
+      Loop
+    End If
+
+    End Sub
+
+  
+  Sub ListOutput()
+        'Prints info about user's choices
+        Dim r As Random = New Random
+        Dim num As Integer = r.Next(99)
+
+    lstDisplay.Items.Clear()
+    lstDisplay.Items.Add("")
+    lstDisplay.Items.Add("Date: " & Today)
+    lstDisplay.Items.Add("")
+    lstDisplay.Items.Add("You can buy with your $" & input & ":")
+
+    'Prints info about $5 tickets only if they were selected by user.
+    If chkBingo.Checked Or chkMega.Checked Then
+      lstDisplay.Items.Add(ticket1.ToString &
+                           " Bingo Gold or Mega Millions lottery tickets.")
+    End If
+
+    'Prints info about $2 tickets only if they were selected by user.
+    If chkInferno.Checked Or chkPowerball.Checked Then
+      lstDisplay.Items.Add(ticket2 &
+                           " Cash Inferno and/or Powerball lottery tickets.")
+    End If
+
+    'Prints info about $1 tickets only if they were selected by user.
+    If chkFlorida.Checked Or chkRoyal.Checked Then
+      lstDisplay.Items.Add(ticket3.ToString &
+                           " Royal 8's or Florida Lottery tickets.")
+    End If
+
+    'Prints change info only if there is any.
+    If change > 0 Then
+            lstDisplay.Items.Add("Your change is: $" & change.ToString("n"))
+    End If
+
+    lstDisplay.Items.Add("")
+
+    'Creates a loop to generate i amount of lucky numbers with the amount of tickets you can buy
+    For i = 1 To Math.Max(Math.Max(ticket1, ticket2), ticket3)
+      lstDisplay.Items.Add("Lucky numbers: " & r.Next(99) & ", " & r.Next(99) &
+                           ", " & r.Next(99) & ", " & r.Next(99) & ", " & r.Next(99))
+    Next i
+
+    lstDisplay.Items.Add("")
+    lstDisplay.Items.Add("Good Luck!")
+
+    End Sub
+
+End Class
